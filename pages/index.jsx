3367ec72090f4fa93288ee5c0e542198a86bbf4f
Bryan Yan <74737261+bryanyanay@users.noreply.github.com>
2022-12-27 17:45:34 -0500

    Homepage style changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Home() {
 	return (
 		<div className="p-12 text-black"> 
-			<h1 className="text-6xl font-bold">Easy power transfers between electric vehicles.</h1>
+			<h1 className="text-6xl font-bold leading-relaxed">Easy power transfers between electric vehicles.</h1>
 			<Link href="/signin" className="block mt-28 text-4xl font-thin hover:underline">Get started now.</Link>
 		</div>
 	);
